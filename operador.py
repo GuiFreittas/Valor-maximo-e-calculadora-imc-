4eb0b11f908854176ec0d6ef0215516e5cc98a2d
GuiFreittas <fbguiedits26@gmail.com>
2023-03-06 21:37:15 -0300

    Gui

---
+++ operador.py
@@ -0,0 +1,13 @@
+a = "Lucas"
+b= "18"
+c = "anos"
+
+texto = "O meu nome é: {nome}"
+
+texto2= "Tenh"
+
+exibir = texto.format(nome=a)
+
+
+
+print (exibir)
---
+++ valores.py
@@ -0,0 +1,8 @@
+valor_1 = float(input("Insira o primeiro valor: "))
+valor_2 = float(input("Insira o segundo valor: "))
+
+
+if valor_1 > valor_2:
+    print("O maior valor é:", valor_1)
+else:
+    print("O maior valor é:", valor_2)
